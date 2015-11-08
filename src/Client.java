296b1a057c45e9b066f6aa099437d8e5ad99dd0b
cabewz <cabewz@gmail.com>
2015-11-08 14:53:53 -0500

    Changed to data streams

--- src/Client.java
+++ src/Client.java
@@ -3,23 +3,26 @@ import java.net.*;
 import java.util.Scanner;
 public class Client
 {
+	private static DataInputStream in;
+	private static DataOutputStream out;
+	private static Socket socket;
+
     public static void main(String [] args)
     {
         //Try Catch to handle any exceptions from sockets
         try
         {
             //Creating a socket on the local host with port 8189
-            Socket socket = new Socket("127.0.0.1", 8189);
+            socket = new Socket("127.0.0.1", 8189);
             //Implementing a way to output text to the server
-            OutputStream outStream = socket.getOutputStream();
-            PrintWriter out = new PrintWriter(outStream,true);
+            out = new DataOutputStream(socket.getOutputStream());
             //Input to the client
-            InputStream in = socket.getInputStream();
-            Scanner message = new Scanner(in);
+            in = new DataInputStream(socket.getInputStream());
             //After the connection is initiate the first message is sent.
-            out.println("Connection Initiated");
+            out.writeUTF("Connection Initiated");
+			System.out.println("Message Sent");
             //Prints the input to the console
-            System.out.println(message.nextLine());
+            System.out.println(in.readUTF());
             Scanner textInput = new Scanner(System.in);
             //Keeps looking for input
             boolean done = false;
@@ -28,11 +31,11 @@ public class Client
             while (!done)
             {
                 //The client waits for the next line from the server
-                String lineIn = message.nextLine();
+                String lineIn = in.readUTF();
                 System.out.println(lineIn);
                 //Sending text to the server
                 String text = textInput.nextLine();
-                out.println(text);
+                out.writeUTF(text);
 
                 if(text.equals("exit"))
                 {
